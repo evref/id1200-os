950e8c96a2d4736a2deeb3c0059c8baa213467e1
evref <isak.lefevre@hotmail.se>
2023-11-10 22:23:16 +0100

    improve test script

--- lab3/test.py
+++ lab3/test.py
@@ -1,7 +1,18 @@
 output = open("output.txt", "r")
 correct = open("correct.txt", "r")
 
-if(output.read() == correct.read()):
-    print("Output is correct")
+output_text = output.read().split()
+correct_text = correct.read().split()
+
+if(len(output_text) == len(correct_text)):
+    print("SUCCESS: Number of rows are the same")
 else:
-    print("Output does not match")
+    print("ERROR: Number of rows are not the same")
+
+no_correct = 0
+for i in range(len(correct_text)):
+    if output_text[i] == correct_text[i]:
+        no_correct += 1
+
+print(f"{100 * no_correct / len(correct_text)}% of rows were correct!")
+
